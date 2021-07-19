df84a70637d62747aba5a9cba3ebf171bbeace7e
Saravanan Sathiyamoorthi <developer.saro@gmail.com>
2021-07-19 08:16:51 +0530

    Add files via upload

--- script/mail.js
+++ script/mail.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function mailform() {
   let mailform = document.getElementById("mailform");
   let username = document.getElementById("mailform").name.value;
   console.log("user-name = ", username);
@@ -8,13 +8,39 @@ function myFunction() {
   console.log("user-message = ", usermessage);
   localStorage.setItem("username", mailform.name.value);
   localStorage.setItem("useremail", mailform.email.value);
-  localStorage.setItem("message", mailform.message.value);
+  localStorage.setItem("usermessage", mailform.message.value);
   console.log("localstorage", localStorage.getItem("username"));
 
   return username, useremail, usermessage;
 }
 
-// done by saravanan sathiyamoorthi
 let mailresponse = document.getElementById("mailresponse");
+let replyname = document.querySelector("#replyname").value;
 console.log(mailresponse);
-let;
+let localname = localStorage.getItem("username");
+let localemail = localStorage.getItem("useremail");
+let localmessage = localStorage.getItem("usermessage");
+console.log("localstorage user-name = ", localStorage.getItem("username"));
+console.log("localstorage user-email = ", localStorage.getItem("useremail"));
+console.log(
+  "localstorage user-message = ",
+  localStorage.getItem("usermessage")
+);
+replyname = document.querySelector("#replyname");
+replyname.innerHTML = localname;
+
+const sendemail = () => {
+  Email.send({
+    Host: "smtp.google.com",
+    Username: "developer.saro@gmail.com",
+    Password: "@qwe123R",
+    To: "developer.saro@gmail.com ",
+    From: "developer.saro@gmail.com",
+    Subject: `${localname} sent you a message`,
+    Body: `name : ${localname} <br/> email : ${localemail} <br/> message : ${localmessage}`,
+  }).then((message) => alert("message sent"));
+};
+
+sendemail(localname, localemail, localmessage);
+
+// done by saravanan sathiyamoorthi
